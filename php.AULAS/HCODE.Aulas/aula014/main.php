367ef74df503b648104723e8fd68514c179999f5
LutekDev <lutekdev@gmail.com>
2024-02-19 21:15:21 -0300

    📦 NEW: Added New Files in 19/02/24

--- php.AULAS/HCODE.Aulas/aula014/main.php
+++ php.AULAS/HCODE.Aulas/aula014/main.php
@@ -19,8 +19,9 @@ echo ("\n\n");
 
 // Operadores Aritimeticos
 
-$a = 10;
-$b = 2;
+$a = 2;
+$b = 10;
+$c = NULL;
 
 echo $a + $b; // Soma
 echo ("\n");
@@ -66,3 +67,75 @@ echo "\n";
 
 var_dump($a !== $b); // Diferente de Tipos
 echo "\n\n";
+
+// SpaceShip
+
+var_dump($a <=> $b);
+// Faz uma comparação entre os valores se for maior retorna o $a se for menor retorna $b se forem iguais retorna 0
+
+// $a => 1;
+// $b => -1
+// == => 0
+
+echo "\n\n";
+
+// Null Coalescing
+
+echo $a ?? $b ?? $c; // Sempre para num valor verdadeiro e retorna o valor
+
+/**
+ * $a = NULL;
+ * $b = NULL;
+ * $c = 10;
+ * 
+ * echo $a ?? $b ?? $c;
+ * 
+ * Resultado é $c pois é o unico verdadeiro = 10.
+ */
+
+echo "\n\n";
+
+
+// Operador de Incremento e Decremento
+
+echo $a++; // Incremento Post
+echo "\n";
+
+echo $a;
+
+echo "\n\n";
+
+echo $b--; // Decremento Post
+echo "\n";
+
+echo $b;
+
+echo "\n\n";
+
+echo ++$a; // Incremento Pre
+echo "\n";
+
+echo $a;
+
+echo "\n\n";
+
+echo --$b; // Decremento Pre
+echo "\n";
+
+echo $b;
+
+echo "\n\n";
+
+
+/** 
+ * A Diferença entre o Incremento Antes e Depois:
+ * 
+ * Antes: ++$a;
+ * Depois: $a++;
+ * 
+ * Antes:
+ *  - Faz com que o valor seja incrementado antes da atribuição ou seja antes de ser calculado no programa;
+ * 
+ * Depois:
+ *  - Faz com que o valor seja incrementado depois da atribuição ou seja depois de ser calculado no programa.
+ */

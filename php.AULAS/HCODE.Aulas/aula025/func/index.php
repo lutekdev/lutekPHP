5fe60d21c13bbe8ac7a409a481e42da086b9f100
LutekDev <lutekdev@gmail.com>
2024-02-29 22:01:45 -0300

    📦 NEW: Added New Files In 29/02/2024

--- php.AULAS/HCODE.Aulas/aula025/func/index.php
+++ php.AULAS/HCODE.Aulas/aula025/func/index.php
@@ -1,6 +1,15 @@
 <?php
 
+// require "C:\Users\LutekDev\VSCode Projects\LANGUAGES.PROGRAMMING\PHP\lutekPHP\php.AULAS\HCODE.Aulas\aula025\config\config.php";
+
 function somar($a, $b)
 {
-    return PHP_EOL . "A Soma Deu: " . $a + $b . PHP_EOL;
+    return PHP_EOL . "A Soma Deu: " . ($a + $b) . PHP_EOL;
+}
+
+function generateNewId()
+{
+    ob_start();
+    session_regenerate_id(true);
+    ob_end_flush();
 }
--- php.AULAS/HCODE.Aulas/aula025/main.php
+++ php.AULAS/HCODE.Aulas/aula025/main.php
@@ -7,3 +7,12 @@ $_SESSION["nome"] = "LutekInc";
 
 print "O nome da 'Session' é: " . $_SESSION["nome"];
 print somar(5, 2);
+
+print "===================" . PHP_EOL;
+
+print "O ID de Sessão do Usuario eh: " . session_id() . PHP_EOL;
+
+print "===================" . PHP_EOL;
+
+// session_unset();
+// session_destroy();
